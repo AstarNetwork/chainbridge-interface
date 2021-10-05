8a5c87ced9486b7d77d6396f0800eb49805ee652
Hyungsuk Kang <hskang9@github.com>
2021-10-06 00:54:35 +0900

    initial commit

---
+++ src/Themes/LightTheme.ts
@@ -0,0 +1,115 @@
+import { createTheme } from "@chainsafe/common-theme";
+
+export const lightTheme = createTheme({
+  globalStyling: {
+    body: {
+      backgroundColor: "#F5F5F5",
+      color: "#7C99B9",
+    },
+  },
+  themeConfig: {
+    constants: {
+      navItemHeight: 42,
+    },
+    palette: {
+      additional: {
+        general: {
+          1: "#85A5FF", // Accents //geekblue4
+        },
+        transferUi: {
+          1: "#631bc1", // FAQ button // gray8
+        },
+        header: {
+          1: "#F5F5F5", // Background
+          2: "#631bc1", // Text color //gray8
+          3: "#BFBFBF", // border // gray6
+        },
+        preflight: {
+          1: "#85A5FF", // Button bg color
+          2: "#262626", // Button color
+        },
+        transactionModal: {
+          1: "#597EF7", // border //geekblue5
+          2: "#85A5FF", // indicator border //geekblue4
+          3: "#2F54EB", // indicator text //geekblue6
+        },
+      },
+    },
+    overrides: {
+      CheckboxInput: {
+        root: {
+          alignItems: "center",
+        },
+      },
+      Button: {
+        variants: {
+          primary: {
+            root: {
+              backgroundColor: "#550e8a",
+              color: "#ffffff",
+              border: `1px solid #550e8a`,
+              "& svg": {
+                fill: "#ffffff",
+              },
+            },
+            active: {
+              backgroundColor: "#550e8a",
+              color: "#ffffff",
+              "& svg": {
+                fill: "#ffffff",
+              },
+            },
+            hover: {
+              backgroundColor: "#550e8a",
+              color: "#ffffff",
+              "& svg": {
+                fill: "#ffffff",
+              },
+            },
+            focus: {
+              backgroundColor: "#ffffff",
+              color: "#262626",
+              "& svg": {
+                fill: "#262626",
+              },
+            },
+          },
+          outline: {
+            root: {
+              backgroundColor: "transparent",
+              color: "#ffffff",
+              border: `1px solid #ffffff`,
+              "& svg": {
+                fill: "#ffffff",
+              },
+            },
+            active: {
+              backgroundColor: "#ffffff",
+              color: "#262626",
+              borderColor: "#ffffff",
+              "& svg": {
+                fill: "#262626",
+              },
+            },
+            hover: {
+              backgroundColor: "#ffffff",
+              color: "#262626",
+              borderColor: "#ffffff",
+              "& svg": {
+                fill: "#262626",
+              },
+            },
+            focus: {
+              backgroundColor: "#ffffff",
+              color: "#262626",
+              borderColor: "#ffffff",
+              "& svg": {
+                fill: "#262626",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
